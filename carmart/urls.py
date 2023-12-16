094221fac8b02f86d2416286f36f5e30658c22bc
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-16 11:41:11 +0600

    routes added

--- carmart/urls.py
+++ carmart/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from carmart.views import index, register, user_login, user_logout
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", index),
+    path("carmart/", include("car.urls")),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("signup/", register, name="signup"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
