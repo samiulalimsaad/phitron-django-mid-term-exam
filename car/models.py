afec4a7a08d782314da9ec115071157c28c92cce
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-16 23:38:32 +0600

    quantity added

--- car/models.py
+++ car/models.py
@@ -10,6 +10,7 @@ class Car(models.Model):
     description = models.TextField()
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="car/car_images/", null=True, blank=True)
+    quantity = models.PositiveIntegerField(default=0)  # Add the quantity field
 
     def __str__(self):
         return f"{self.name} - {self.brand}"
@@ -19,6 +20,11 @@ class Buy(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     purchase_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.car.quantity -= 1
+        self.car.save()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.username} bought {self.car.name} on {self.purchase_date}"
